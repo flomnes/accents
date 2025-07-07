caab28d5fb60cf53a459755e1a308276cd2adb67
Florian OMNES <florian.omnes@rte-france.com>
2025-07-07 11:44:42 +0200

    Add myu8path

--- main.cpp
+++ main.cpp
@@ -1,18 +1,31 @@
+#include <cstring>
 #include <filesystem>
 #include <iomanip>
 #include <iostream>
 
+#define FILENAME "josé.txt"
+
 namespace fs = std::filesystem;
+
+fs::path myu8path(const char* s)
+{
+    std::u8string u8s(s, s + std::strlen(s));
+    return fs::path(u8s);
+}
 
 int main()
 {
     std::cout << std::boolalpha;
     {
-        fs::path p("josé.txt");
+        fs::path p(FILENAME);
         std::cout << "fs::path " << fs::exists(p) << std::endl;
     }
     {
-        fs::path q = fs::u8path("josé.txt");
+        fs::path q = fs::u8path(FILENAME);
         std::cout << "fs::u8path " << fs::exists(q) << std::endl;
+    }
+    {
+        fs::path q = fs::path(myu8path(FILENAME));
+        std::cout << "myu8path " << fs::exists(q) << std::endl;
     }
 }
